ac014192eda6a90690c1b1180bb5b6cf3ad8f2e9
folajindayo <temidayofolajin@yahoo.com>
2025-11-17 22:39:57 +0100

    Create proper barrel exports structure

---
+++ frontend/src/shared/exports/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @title Barrel Exports
+ * @description Proper barrel exports structure
+ */
+
+export * from './config/AppConfig';
+export * from './di/DependencyContainer';
+export * from './state/StateManager';
+export * from './api/ApiClient';
+export * from './errors/ErrorBoundaryWrapper';
+export * from './routing/LazyRoutes';
+export * from './code-splitting/CodeSplitter';
+
