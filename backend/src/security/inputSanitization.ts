a454c83c83f8a31e07f0f0fe0952e9d2c669dfda
folajindayo <temidayofolajin@yahoo.com>
2025-11-17 22:50:34 +0100

    Create input sanitization layer

---
+++ backend/src/security/inputSanitization.ts
@@ -0,0 +1,9 @@
+/** Input Sanitization Layer */
+export const sanitize = (input: string) => {
+  return input
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#x27;');
+};
+
