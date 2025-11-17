b3c8dde4557cd0db2d9120d0e2e34ee1ccd3d926
folajindayo <temidayofolajin@yahoo.com>
2025-11-17 22:45:18 +0100

    Add transaction management

---
+++ backend/src/database/transaction.ts
@@ -0,0 +1,17 @@
+/** Transaction Management */
+import { pool } from './connection';
+export const withTransaction = async (callback: (client: any) => Promise<any>) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const result = await callback(client);
+    await client.query('COMMIT');
+    return result;
+  } catch (err) {
+    await client.query('ROLLBACK');
+    throw err;
+  } finally {
+    client.release();
+  }
+};
+
