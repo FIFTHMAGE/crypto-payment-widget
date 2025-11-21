bc99d2884d2f456e19c0d61fe9d1e8eb685a146b
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-21 08:24:09 +0100

    Remove authorization middleware from the backend

--- backend/src/middleware/authMiddleware.ts
+++
@@ -1,7 +0,0 @@
-/** Authorization Middleware */
-export const authorize = (requiredRole: string) => (req: any, res: any, next: any) => {
-  const userRole = req.user?.role;
-  if (userRole !== requiredRole) return res.status(403).json({ error: 'Forbidden' });
-  next();
-};
-
