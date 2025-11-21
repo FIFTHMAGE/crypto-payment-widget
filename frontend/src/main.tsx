0d6e75dec80c2e74c4b40621ce0ccf75b2d3c34b
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-21 17:35:00 +0100

    Add React application entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import AppKitProvider from './context/AppKitProvider.tsx'
-import { ErrorBoundary } from './components/ui'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <AppKitProvider>
-        <App />
-      </AppKitProvider>
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
-
+    <App />
+  </React.StrictMode>
+);
