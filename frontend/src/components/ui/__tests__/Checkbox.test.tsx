3a59a334c360de36d3b650cb609b46a61a94665b
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-25 22:58:01 +0100

    refactor(code): standardize imports in Checkbox.test.tsx

--- frontend/src/components/ui/__tests__/Checkbox.test.tsx
+++ frontend/src/components/ui/__tests__/Checkbox.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi } from 'vitest'
-import { render, screen } from '../../../test/utils/test-utils'
+
 import { Checkbox } from '../Checkbox'
+import { render, screen } from '../../../test/utils/test-utils'
 
 describe('Checkbox', () => {
   it('should render checkbox', () => {
