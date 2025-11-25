6663c879e798ca456b69c46399716aeb9826ec85
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-25 22:57:15 +0100

    refactor(code): standardize imports in transactionService.test.js

--- backend/src/services/__tests__/transactionService.test.js
+++ backend/src/services/__tests__/transactionService.test.js
@@ -1,5 +1,6 @@
-import { describe, it, expect, beforeEach } from '@jest/globals'
 import {
+import { describe, it, expect, beforeEach } from '@jest/globals'
+
   createTransaction,
   getTransactions,
   getTransactionByHash,
