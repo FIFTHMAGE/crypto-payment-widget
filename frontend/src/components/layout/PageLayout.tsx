c67782f95dd12b95d0e6a527a35bfc835a84aec0
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-25 22:57:49 +0100

    refactor(code): standardize imports in PageLayout.tsx

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Header } from './Header'
-import { Footer } from './Footer'
 import { Container } from './Container'
+import { Footer } from './Footer'
+import { Header } from './Header'
 
 interface PageLayoutProps {
   children: React.ReactNode
