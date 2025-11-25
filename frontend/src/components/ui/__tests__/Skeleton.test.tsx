5b788f2633ab82d47ffd43605382634421b4477e
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-25 22:58:06 +0100

    refactor(code): standardize imports in Skeleton.test.tsx

--- frontend/src/components/ui/__tests__/Skeleton.test.tsx
+++ frontend/src/components/ui/__tests__/Skeleton.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest'
-import { render } from '../../../test/utils/test-utils'
+
 import { Skeleton } from '../Skeleton'
+import { render } from '../../../test/utils/test-utils'
 
 describe('Skeleton', () => {
   it('should render skeleton loader', () => {
